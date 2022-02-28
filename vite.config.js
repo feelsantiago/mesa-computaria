5c2b98941d0df454aa1ed63c7460c9402636c3fe
Filipe Santiago <porto.santiago@hotmail.com>
2022-02-28 19:18:00 -0300

    create base architecutre

---
+++ vite.config.js
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vite';
+import path from 'path';
+import htmlTemplate from 'vite-plugin-html-template';
+import mpa from 'vite-plugin-mpa';
+
+export default defineConfig({
+    plugins: [
+        mpa({
+            open: '/home',
+        }),
+        htmlTemplate({
+            pagesDir: 'src/pages',
+            pages: {
+                home: {
+                    template: './src/layouts/home.layout.html',
+                    title: 'HomePage',
+                },
+            },
+            data: {
+                title: 'HomePage',
+            },
+        }),
+    ],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+        },
+    },
+});
