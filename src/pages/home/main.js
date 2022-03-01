ea8e9ce1fee243af0a0d7231b11feef6d80378fa
Filipe Santiago <porto.santiago@hotmail.com>
2022-03-01 13:40:22 -0300

    add unit test

--- src/pages/home/main.js
+++ src/pages/home/main.js
@@ -1,1 +1,7 @@
 import './style.scss';
+
+function sum(a, b) {
+    return a + b;
+}
+
+export default sum;
--- vite.config.js
+++ vite.config.js
@@ -26,4 +26,9 @@ export default defineConfig({
             '@': path.resolve(__dirname, './src'),
         },
     },
+    test: {
+        globals: true,
+        reporters: 'verbose',
+        watch: false
+    }
 });
