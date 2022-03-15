e48245a021c21cd3803f8ee22abd3a2f79bd560f
pedroemanuel42 <99288851+pedroemanuel42@users.noreply.github.com>
2022-03-15 18:00:37 -0300

    [Feature]MC-15 Testing function

--- src/services/password-input.service.js
+++ src/services/password-input.service.js
@@ -4,7 +4,7 @@ export default class PasswordInput {
     initFor(queryIcon) {
         const toggleIcon = $(queryIcon);
         toggleIcon.on('click', () => {
-            if ( $('#pw_input').attr('type') === 'password' ) {
+            if ($('#pw_input').attr('type') == 'password') {
                 $('#pw_input').attr('type', 'text');
                 $('#eye_icon').attr('class', 'fa-solid fa-eye');
               } else {
--- src/services/tests/password-input.service.test.js
+++ src/services/tests/password-input.service.test.js
@@ -8,17 +8,40 @@ describe('[PasswordInputService]', () => {
 
         it('Should add a listener to click event', () => {
 
-            const input = '<input id="test-input">';
+            const input = '<input id="input-id-test">';
             document.body.innerHTML = input;
 
             vi.spyOn($.fn, 'on');
-            PasswordInputService.initFor('#test-input');
+            PasswordInputService.initFor('#input-id-test');
 
             expect($.fn.on).toHaveBeenCalledWith('click', expect.anything());
             expect($.fn.on).toHaveBeenCalledTimes(1);
         });
 
-        it('Should change password input to text input', () => {});
+        it('Should change password input to text input', () => {
+        
+            const input = '<input type="password" id="input-id-test">';
+            document.body.innerHTML = input;
 
+            PasswordInputService.initFor('#input-id-test');
+            $('#input-id-test').trigger('click');
+
+            const result = $('#input-id-test').attr('type');
+            
+            expect(result).toBe('text');
+        });
+
+        it('Should change text input to password input', () => {
+        
+            const input = '<input type="text" id="input-id-test">';
+            document.body.innerHTML = input;
+
+            PasswordInputService.initFor('#input-id-test');
+            $('#input-id-test').trigger('click');
+
+            const result = $('#input-id-test').attr('type');
+
+            expect(result).toBe('password');
+        });
     });
 });
