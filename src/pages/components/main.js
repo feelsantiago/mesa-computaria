ce2b00fe0b542e66ae6334e433e495883b89dfb2
EvertonDuarteGuimarães <everton.duarte@academico.ifpb.edu.br>
2022-03-21 20:46:12 -0300

    MC-24 First commit

--- src/pages/components/main.js
+++ src/pages/components/main.js
@@ -1,5 +1,7 @@
 import { LayoutProviderService } from '../../services/layout-provider.service';
 
+import { DropdownService } from '../../services/dropdown.service';
+
 import './style.scss';
 import page from './index.html?raw';
 
@@ -10,5 +12,7 @@ export default function sum(a, b) {
 LayoutProviderService.inject(page).then(() => {
     // Javascript Code initialization
     sum(1, 2);
-});
 
+    DropdownService.toggleList();
+    DropdownService.itemSelect();
+});
---
+++ src/services/tests/dropdown.service.test.js
@@ -0,0 +1,3 @@
+import { describe, expect, it, vi } from 'vitest';
+import $ from 'jquery';
+import { LayoutProviderService } from '../layout-provider.service';
