d1620ae78c63a3ece8506a95666ebab2ec921e39
Filipe Santiago <porto.santiago@hotmail.com>
2022-03-28 11:36:40 -0300

    MC-37 Throw error if inject content in test mode

--- src/pages/components/main.js
+++ src/pages/components/main.js
@@ -3,8 +3,16 @@ import { PasswordInputService } from '../../services/password-input.service';
 
 import './style.scss';
 import page from './index.html?raw';
+import { TestEnvironmentError } from '../../utils/errors';
 
-LayoutProviderService.inject(page).then(() => {
-    // Javascript Code initialization
-    PasswordInputService.initFor('#password');
-});
+LayoutProviderService.inject(page)
+    .then(() => {
+        // Javascript Code initialization
+        PasswordInputService.initFor('#password');
+    })
+    .catch((error) => {
+        if (!(error instanceof TestEnvironmentError)) {
+            // eslint-disable-next-line no-console
+            console.log(error);
+        }
+    });
--- src/services/tests/layout-provider.service.test.js
+++ src/services/tests/layout-provider.service.test.js
@@ -1,6 +1,7 @@
 import { describe, expect, it, vi } from 'vitest';
 import $ from 'jquery';
 import { LayoutProviderService } from '../layout-provider.service';
+import { TestEnvironmentError } from '../../utils/errors';
 
 describe('[LayoutProviderService]', () => {
     describe('[inject]', () => {
@@ -9,7 +10,7 @@ describe('[LayoutProviderService]', () => {
             vi.spyOn($.fn, 'html');
 
             document.body.innerHTML = '<div id="app"></div>';
-            await LayoutProviderService.inject(`<h1>${content}</h1>`);
+            await LayoutProviderService.inject(`<h1>${content}</h1>`, true);
 
             expect($.fn.html).toBeCalledTimes(1);
             expect($('#app h1').html()).toBe('Test Mock Page');
@@ -21,6 +22,15 @@ describe('[LayoutProviderService]', () => {
             } catch (error) {
                 expect(error).toBeInstanceOf(Error);
                 expect(error.message).toBeDefined();
+            }
+        });
+
+        it('Should throw error if running in test environment', async () => {
+            try {
+                await LayoutProviderService.inject('test page content');
+            } catch (error) {
+                expect(error).toBeDefined();
+                expect(error).toBeInstanceOf(TestEnvironmentError);
             }
         });
     });
