9fc8b3a55a5b90a7dbc1c425054c76fc411aab5d
EvertonDuarteGuimarães <everton.duarte@academico.ifpb.edu.br>
2022-03-30 17:27:21 -0300

    MC-24 import vi removed

--- src/components/tests/dropdown.component.test.js
+++ src/components/tests/dropdown.component.test.js
@@ -1,4 +1,4 @@
-import { beforeEach, describe, expect, it, vi, afterEach } from 'vitest';
+import { beforeEach, describe, expect, it, afterEach } from 'vitest';
 import $ from 'jquery';
 import DropdownComponent from '../dropdown.component';
 
