4a533004b8f3de9fdd30e118bb8ba1f84e870c9b
EvertonDuarteGuimarães <everton.duarte@academico.ifpb.edu.br>
2022-04-05 15:37:55 -0300

    MC-42 add setEnable/Disable and isDisabled

--- src/components/dropdown.component.js
+++ src/components/dropdown.component.js
@@ -4,13 +4,57 @@ export default class DropdownComponent {
     constructor(query) {
         this.query = query;
 
+        this.dropdownContainer = $(`${query} .dropdown-container`);
         this.btn = $(`${query} .dropdown-btn`);
         this.items = $(`${query} .dropdown-item`);
         this.list = $(`${query} .dropdown-list`);
         this.selectedItem = $(`${query} .selected`);
 
+        /* codes between theses coments are made just for test */
+        this.enableButton = $(`${query} #e`);
+        this.disableButton = $(`${query} #d`);
+        this.clickE();
+        this.clickD();
+        /* codes between theses coments are made just for test */
+
         this.addDropdownClickEventListner();
         this.addItemClickEventListner();
+    }
+
+    /* codes between theses coments are made just for test */
+    clickE() {
+        this.enableButton.on('click', () => {
+            this.setEnabled();
+            console.log(this.isDisabled());
+            console.log(this.dropdownContainer.attr('disabled'));
+        });
+    }
+
+    clickD() {
+        this.disableButton.on('click', () => {
+            this.setDisabled();
+            console.log(this.isDisabled());
+            console.log(this.dropdownContainer.attr('disabled'));
+        });
+    }
+    /* codes between theses coments are made just for test */
+
+    setEnabled() {
+        this.dropdownContainer.prop('disabled', false);
+        this.dropdownContainer.removeClass('disabled');
+    }
+
+    setDisabled() {
+        this.dropdownContainer.prop('disabled', true);
+        this.dropdownContainer.addClass('disabled');
+    }
+
+    isDisabled() {
+        if (this.dropdownContainer.hasClass('disabled')) {
+            return true;
+        }
+
+        return false;
     }
 
     addDropdownClickEventListner() {
@@ -41,3 +85,4 @@ export default class DropdownComponent {
         this.updateDropdown();
     }
 }
+
