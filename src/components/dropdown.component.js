d850a72831ac4a21662fe396577eee391e8e08a3
EvertonDuarteGuimarães <everton.duarte@academico.ifpb.edu.br>
2022-04-05 16:55:11 -0300

    MC-42 fixed set/get dropdown enabled state

--- src/components/dropdown.component.js
+++ src/components/dropdown.component.js
@@ -10,51 +10,26 @@ export default class DropdownComponent {
         this.list = $(`${query} .dropdown-list`);
         this.selectedItem = $(`${query} .selected`);
 
-        /* codes between theses coments are made just for test */
-        this.enableButton = $(`${query} #e`);
-        this.disableButton = $(`${query} #d`);
-        this.clickE();
-        this.clickD();
-        /* codes between theses coments are made just for test */
+        this.enabled = this.dropdownContainer.attr('disabled');
 
         this.addDropdownClickEventListner();
         this.addItemClickEventListner();
     }
 
-    /* codes between theses coments are made just for test */
-    clickE() {
-        this.enableButton.on('click', () => {
-            this.setEnabled();
-            console.log(this.isDisabled());
-            console.log(this.dropdownContainer.attr('disabled'));
-        });
-    }
-
-    clickD() {
-        this.disableButton.on('click', () => {
-            this.setDisabled();
-            console.log(this.isDisabled());
-            console.log(this.dropdownContainer.attr('disabled'));
-        });
-    }
-    /* codes between theses coments are made just for test */
-
-    setEnabled() {
-        this.dropdownContainer.prop('disabled', false);
-        this.dropdownContainer.removeClass('disabled');
-    }
-
-    setDisabled() {
-        this.dropdownContainer.prop('disabled', true);
-        this.dropdownContainer.addClass('disabled');
-    }
-
-    isDisabled() {
-        if (this.dropdownContainer.hasClass('disabled')) {
-            return true;
+    setEnabled(boolean) {
+        this.enabled = this.dropdownContainer.prop('disabled', boolean);
+        if (boolean) {
+            this.dropdownContainer.removeClass('disabled');
+        } else {
+            this.dropdownContainer.addClass('disabled');
         }
+    }
 
-        return false;
+    isEnabled() {
+        if (this.enabled.attr('disabled')) {
+            return false;
+        }
+        return true;
     }
 
     addDropdownClickEventListner() {
@@ -85,4 +60,3 @@ export default class DropdownComponent {
         this.updateDropdown();
     }
 }
-
