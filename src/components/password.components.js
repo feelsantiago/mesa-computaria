e1a188e251042d7089532040e7ed3923394667d6
pedroemanuel42 <99288851+pedroemanuel42@users.noreply.github.com>
2022-04-06 19:25:38 -0300

    MC-40 Refactored valid,  required and disabled

--- src/components/password.components.js
+++ src/components/password.components.js
@@ -1,25 +1,17 @@
 import $ from 'jquery';
 
 export default class PasswordInput {
-    constructor(queryString) {
+    constructor(queryString, valid = false, required = true, disabled = false) {
         this.div = $(queryString);
         this.input = $(`${queryString} input[type="password"]`);
         this.icon = $(`${queryString} input[type="password"]+i`);
 
-        this.valid = false;
-        this.enabled = this.input.attr('disabled', false);
-        this.required = this.input.attr('required', true);
+        this.valid = valid;
+        this.disabled = disabled;
+        this.required = required;
 
         this.hasQuery(this.div, this.input, this.icon);
         this.initFor();
-    }
-
-    getValue() {
-        return this.input.val();
-    }
-
-    setValue(string) {
-        this.input.val(string);
     }
 
     hasQuery(div, input, icon) {
@@ -47,21 +39,20 @@ export default class PasswordInput {
             }
         });
     }
-
-    getValid() {
-        return this.valid;
+    
+    getValue() {
+        return this.input.val();
     }
 
-    setValid(boolean) {
-        this.valid = boolean;
+    setValue(string) {
+        this.input.val(string);
     }
 
     isValid() {
-        // Validate regex: at least 1 capital letter, 1 number, 1 special character, and size 8;
-        const validations = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*(),.?":{}|<>])[A-Za-z\d!@#$%^&*(),.?":{}|<>]{8,}$/;
-        const value = this.input.val();
+        // Validate regex: 1 capital letter, 1 number, 1 special character, and size 8;
+        const validations = /^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*(),.?":{}|<>])[A-Za-z\d!@#$%^&*(),.?":{}|<>]{8,}$/; 
 
-        if (validations.test(value)) {
+        if (validations.test(this.input.val())) {
             this.setValid(true);
             return this.valid;
         }
@@ -69,21 +60,22 @@ export default class PasswordInput {
         return this.valid;
     }
 
-    getEnabled() {
-        return this.enabled;
+    getDisabled() {
+        return this.disabled;
     }
 
-    setEnabled(boolean) {
+    setDisabled(boolean) {
         this.input.prop('disable', boolean);
-        this.enabled = boolean;
+        this.disabled = boolean;
     }
 
-    isEnabled() {
-        if (this.enabled) {
-            return true;
+    isDisabled() {
+        if (this.input.prop('disabled', true)) {
+            this.disabled = true;
+            return this.disabled;
         }
 
-        return false;
+        return this.disabled;
     }
 
     getRequired() {
@@ -91,15 +83,16 @@ export default class PasswordInput {
     }
 
     setRequired(boolean) {
-        this.input.prop('disable', boolean);
+        this.input.prop('required', boolean);
         this.required = boolean;
     }
 
     isRequired() {
-        if (this.required) {
-            return true;
+        if (this.input.prop('required', false)) {
+            this.required = false;
+            return this.required;
         }
 
-        return false;
+        return this.required;
     }
 }
