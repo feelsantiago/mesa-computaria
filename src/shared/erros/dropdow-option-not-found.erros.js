9eb05834f687df1cec03f02e0b64fbf8de3d5dd8
EvertonDuarteGuimarães <everton.duarte@academico.ifpb.edu.br>
2022-04-14 10:58:25 -0300

    MC-42 fixe dropdown option not found parameter

--- src/shared/erros/dropdow-option-not-found.erros.js
+++ src/shared/erros/dropdow-option-not-found.erros.js
@@ -1,7 +1,7 @@
 export default class DropdownOptionNotFound extends Error {
-    constructor(notFoundMessage) {
-        super(notFoundMessage);
-        this.message = `${notFoundMessage} are not included in this dropdown`;
+    constructor(option) {
+        super(option);
+        this.message = `${option} are not included in this dropdown`;
         this.name = 'OptionNotFound';
     }
 }
