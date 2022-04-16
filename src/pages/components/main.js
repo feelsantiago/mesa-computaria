951d1ed66aaf7e15166444137462592c39faca88
RodrigoSoaresNascimento <rodrigosnascimento1@gmail.com>
2022-04-16 14:37:25 -0300

    MC-45 adjusting the imports in main.js

--- src/pages/components/main.js
+++ src/pages/components/main.js
@@ -1,17 +1,21 @@
 import { LayoutProviderService } from '../../services/layout-provider.service';
 import { PasswordInputService } from '../../services/password-input.service';
-import Input from '../../components/text-input.components';
+import TextInput from '../../components/text-input.components';
+import EmailTextInput from '../../components/text-input-email.components';
 
 import DropdownComponent from '../../components/dropdown.component';
 
 import './style.scss';
 import page from './index.html?raw';
 
+
 LayoutProviderService.inject(page).then(() => {
     // eslint-disable-next-line no-unused-vars
     const dropdownGameType = new DropdownComponent('#games');
     PasswordInputService.initFor('#password');
     // javascript text-input-box initialization
-    const validateTextInput = new Input('.container-text-input-box');
+    const validateTextInput = new TextInput('.container-text-input-box');
     validateTextInput.isTextValid();
+    const validateEmailInput = new EmailTextInput('.container-text-input-box');
+    
 });
