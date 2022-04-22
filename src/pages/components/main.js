358f973460ea85df1604c91e486e7140bed13e6e
RodrigoSoaresNascimento <rodrigosnascimento1@gmail.com>
2022-04-22 18:33:36 -0300

    MC-45 fixing a unused consts

--- src/pages/components/main.js
+++ src/pages/components/main.js
@@ -1,5 +1,4 @@
 import { LayoutProviderService } from '../../services/layout-provider.service';
-import { PasswordInputService } from '../../services/password-input.service';
 import TextInput from '../../components/text-input.components';
 import EmailTextInput from '../../components/text-input-email.components';
 
@@ -14,6 +13,7 @@ LayoutProviderService.inject(page).then(() => {
     const validateTextInput = new TextInput('.container-text-input-box');
     // eslint-disable-next-line no-unused-vars
     const validateEmailInput = new EmailTextInput('.container-text-input-box');
+    // eslint-disable-next-line no-unused-vars
     const password = new PasswordInput('#password');
     // eslint-disable-next-line no-unused-vars
     const dropdownGameType = new DropdownComponent('#games', 'Game Type');
